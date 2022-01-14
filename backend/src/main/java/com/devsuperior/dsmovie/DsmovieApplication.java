f860f11d31270b15f9ce837f507b753f0398ceb7
Matheus Oliveira <matheus_0119@outlook.com>
2022-01-14 15:41:22 -0300

    Projeto criado na semana Spring React da plataforma DevSuperior

--- backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dsmovie;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
